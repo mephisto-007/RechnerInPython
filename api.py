593481b3ffcd56750489e86ac181e1a5b3787608
mephisto-007 <fleermaxim@gmail.com>
2025-10-01 09:51:49 +0200

    updatet Error Code Raising

--- api.py
+++ api.py
@@ -104,7 +104,7 @@ async def convert(from_currency: str, to_currency: str, amount: float):
         response = await client.get(BASE_URL, params=params)
 
     if response.status_code != 200:
-        raise HTTPException(status_code=500, detail=f"API-Anfrage fehlgeschlagen: {response.status_code}")
+        raise HTTPException(status_code=response.status_code, detail=f"API-Anfrage fehlgeschlagen: {response.status_code}")#The error 
 
     data = response.json()
     if not data.get("success", False):
