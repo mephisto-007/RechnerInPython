578667aba9d0f85770e6b0b7f0cb3eb04b977686
mephisto-007 <fleermaxim@gmail.com>
2025-10-02 19:20:57 +0200

    added menu point and logic for Zinsrechner

--- Hauptprogramm.py
+++ Hauptprogramm.py
@@ -2,8 +2,13 @@ import subprocess
 while True:
     print("1: Rechner")
     print("2: Währungsumrechner")
-    print("3: Beenden")
+    print("3: Zinsrechner")
+    print("4: Beenden")
     choice = int(input("Bitte wählen Sie eine Option: "))
+    
+    if choice == 4:
+        print("Programm wird beendet.")
+        break
     match choice:
         case 1:
             subprocess.run(["python", "Rechner.py"])
@@ -12,8 +17,7 @@ while True:
             uvicorn_process = subprocess.Popen(["uvicorn", "api:app"])
 
         case 3:
-            print("Programm wird beendet.")
-            break
+            subprocess.run(["python", "Zinsrechner.py"])
         
         case _:
             print("Ungültige Auswahl. Bitte erneut versuchen.")
